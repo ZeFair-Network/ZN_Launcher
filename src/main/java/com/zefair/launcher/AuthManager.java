766f7133e5d110e6848d50f2c3c155199604d117
karin <karintexxx@inbox.ru>
2024-11-04 00:05:22 +0300

    First update

---
+++ src/main/java/com/zefair/launcher/AuthManager.java
@@ -0,0 +1,8 @@
+package com.zefair.launcher;
+
+public class AuthManager {
+    public  String authenticate(String username, String passsword) {
+        // Пока не забыл, сюда код аутентификации и возврат токена доступа
+        return "access_token";
+    }
+}
---
+++ src/main/java/com/zefair/launcher/Launcher.java
@@ -0,0 +1,7 @@
+package com.zefair.launcher;
+
+public class Launcher {
+    public static void main(String[] args) {
+
+    }
+}
